b6ccafe29a3299da471ad2e5c0e0acafbf6e3b12
neliocarlos <neliocarlos95@gmail.com>
2022-11-18 21:08:55 -0400

    corrigindo a tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,8 @@
-import { Input } from './components/Input';
-
 const Login = () => {
   return (
-    <div className="md:container flex justify-center items-center h-screen">
-      <div className="bg-verde-insted grid gap-0 grid-cols-1 grid-rows-5 place-content-center rounded-xl h-3/5 mx-auto w-4/5">
-        <div className="bg-logo-insted bg-contain bg-no-repeat content-center h-4/5 w-4/5 mx-auto mt-auto"></div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-verde-insted grid gap-0 grid-cols-1 grid-rows-5 place-content-center rounded-xl h-3/5 w-4/5">
+        <div className="bg-logo-insted bg-contain bg-center bg-no-repeat h-4/5 w-4/5 mx-auto mt-auto"></div>
         <div className="w-full justify-center flex mt-20">
           <input
             type="text"
@@ -12,7 +10,8 @@ const Login = () => {
             className="flex
               justify-center
               items-center
-              w-64
+              mx-auto
+              w-4/5
               h-8
               text-md
               text-white
@@ -33,7 +32,8 @@ const Login = () => {
             className="flex
               justify-center
               items-center
-              w-64
+              mx-auto
+              w-4/5
               h-8
               text-md
               text-white
@@ -47,15 +47,15 @@ const Login = () => {
               border-b"
           />
         </div>
-        <div className="w-full flex justify-center mt-8 h-8">
-          <button className="bg-white shadow-md text-verde-insted text-xl h-12 w-48 rounded-3xl">
+        <div className="w-full flex justify-center items-center mt-8 h-8">
+          <button className="bg-white shadow-md text-verde-insted text-xl h-12 w-40 rounded-3xl">
             LOGIN
           </button>
         </div>
         <div className="flex justify-center items-center w-full">
           <input type="checkbox" className="mx-1" />
           <label className="text-xs text-slate-700 ml-0.5">Continuar conectado?</label>
-          <h4 className="ml-3 text-slate-700 hover:underline text-xs">Esqueci minha senha</h4>
+          <h4 className="ml-3 text-slate-700 hover:underline text-xs">Esqueceu sua senha?</h4>
         </div>
       </div>
     </div>
