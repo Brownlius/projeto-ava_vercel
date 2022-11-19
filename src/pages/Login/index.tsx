7cde3ad04c5f7666847c249eb4ddf085cc965967
neliocarlos <neliocarlos95@gmail.com>
2022-11-18 20:25:58 -0400

    corrigindo a página de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,9 +2,9 @@ import { Input } from './components/Input';
 
 const Login = () => {
   return (
-    <div className="md:container justify-center flex fixed mt-40 mb-40 h-screen">
-      <div className="bg-verde-insted grid gap-0 grid-cols-1 grid-rows-5 justify-center rounded-xl h-3/5 ml-12 w-80">
-        <div className="bg-logo-insted bg-contain bg-no-repeat h-24 w-full mt-7 ml-3"></div>
+    <div className="md:container flex justify-center items-center h-screen">
+      <div className="bg-verde-insted grid gap-0 grid-cols-1 grid-rows-5 place-content-center rounded-xl h-3/5 mx-auto w-4/5">
+        <div className="bg-logo-insted bg-contain bg-no-repeat content-center h-4/5 w-4/5 mx-auto mt-auto"></div>
         <div className="w-full justify-center flex mt-20">
           <input
             type="text"
@@ -52,12 +52,10 @@ const Login = () => {
             LOGIN
           </button>
         </div>
-        <div className="flex justify-center w-full mt-10">
-          <input type="checkbox" className="mr-1 mb-8" />
-          <label className="text-xs text-slate-700 mt-2.5">Continuar conectado?</label>
-          <h4 className="ml-5 mt-2.5 text-slate-700 hover:underline text-xs">
-            Esqueci minha senha
-          </h4>
+        <div className="flex justify-center items-center w-full">
+          <input type="checkbox" className="mx-1" />
+          <label className="text-xs text-slate-700 ml-0.5">Continuar conectado?</label>
+          <h4 className="ml-3 text-slate-700 hover:underline text-xs">Esqueci minha senha</h4>
         </div>
       </div>
     </div>
