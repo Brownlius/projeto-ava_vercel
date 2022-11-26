a4bfbd91b46c16caed07d72bc9783a97911a67a4
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2022-11-26 16:06:05 -0300

    Sugestões de alterações na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,8 +7,8 @@ const Home = () => {
     <>
       <Header />
 
-      <main className="flex flex-wrap justify-center items-center mt-20 h-screen overflow-auto">
-        <div className="bg-verde-insted w-11/12 h-auto flex-wrap rounded-md">
+      <main className="flex flex-wrap justify-center items-center mt-16 pt-4 pb-16 h-screen overflow-auto">
+        <div className="bg-verde-insted w-screen mx-2 h-auto flex-wrap rounded-md">
           <div>
             <h1 className="text-xl text-white ml-3 my-1">Avisos</h1>
           </div>
@@ -19,7 +19,7 @@ const Home = () => {
           />
         </div>
 
-        <div className="bg-verde-insted w-11/12 h-1/5 flex flex-col rounded-md mt-4">
+        <div className="bg-verde-insted w-screen mx-2 h-1/6 flex flex-col rounded-md mt-4">
           <div>
             <h1 className="text-xl text-white ml-3 my-1">Horários</h1>
           </div>
@@ -36,7 +36,7 @@ const Home = () => {
                   subject="Projeto Integrador II"
                   teacher="Ana Cláudia Pedro Ândreo"
                 />
-                <div className="bg-white w-full h-1/4 flex-wrap rounded-sm mx-1 grid grid-cols-3">
+                <div className="bg-white w-full h-1/4 flex-wrap rounded-sm mx-1 grid grid-cols-3 my-1">
                   <div className="justify-center items-center flex col-span-1">
                     <img src="/images/relogio-icon-laranja.png" alt="" className="w-5 h-7 pt-2" />
                     <h1 className="pt-2 pl-1.5"> - 20:40</h1>
@@ -54,9 +54,9 @@ const Home = () => {
             </nav>
           </section>
         </div>
-        <div className="bg-verde-insted w-11/12 h-1/3 flex-wrap rounded-md mt-4"></div>
-        <div className="bg-verde-insted w-11/12 h-1/3 flex-wrap rounded-md mt-4"></div>
-        <div className="bg-verde-insted w-11/12 h-1/3 flex-wrap rounded-md mt-4"></div>
+        <div className="bg-verde-insted w-screen mx-2 h-1/3 flex-wrap rounded-md mt-4"></div>
+        <div className="bg-verde-insted w-screen mx-2 h-1/3 flex-wrap rounded-md mt-4"></div>
+        <div className="bg-verde-insted w-screen mx-2 h-1/3 flex-wrap rounded-md mt-4"></div>
       </main>
       <Footer />
     </>
