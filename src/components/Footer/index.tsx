155078206881b6a61096eceab089cf6932d30d23
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2022-12-11 15:14:33 -0400

    gridado

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex flex-row justify-between items-center fixed inset-x-0 bottom-0 bg-verde-insted ">
+    <footer className="flex flex-row justify-between items-center fixed inset-x-0 bottom-0 bg-verde-insted ">
       <div className="m-2 justify-center items-center flex">
         <img src="/images/menu-button3.png" alt="Calendar button" width={30} />
       </div>
@@ -12,7 +12,7 @@ const Footer = () => {
       <div className=" m-2 justify-center items-center flex">
         <img src="/images/perfil-padrao.png" alt="Clock button" width={30} />
       </div>
-    </div>
+    </footer>
   );
 };
 
