bf45fdc9ff316b1dd33d9cfa54e6b3c668162252
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2022-12-24 12:30:19 -0300

    icons updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'relogio-icon': "url('../public/images/clock.png')"
       },
       colors: {
-        'verde-insted': 'rgb(29,169,173)',
+        'verde-insted': '#1da9ad',
         'verde-insted-icon': 'rgba(138, 220, 222, 1)',
         'verde-texto': 'rgb(5, 40, 40)',
         'texto-input': 'rgba(250,250,250,0.75)',
